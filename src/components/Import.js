719cefc23ba7f07cce7540af045241bfd8853d23
Your Name <you@example.com>
2020-02-24 00:08:26 -0600

    still working

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  Container,
+  TableCell
+} from "@material-ui/core";
 
-const Import = (props) => {
-    // fill out this component
+const Import = props => {
+  return (
+<div>
+    <Container maxWidth="lg" className="car-container">
+    <div>
+    <Button variant="contained" color="primary" onClick={props.fetchMakes}>Import</Button>
+    </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+       
+        <TableBody>
+        {props.makes.map((make, index) => {
+            return (
+            <TableRow key={make.MakeId}>
+              <TableCell>{make.MakeId}</TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell></TableCell>
+            </TableRow>             
+              )
+          })}
+        </TableBody>
+    </Container>
+  </div>
 
-    return (
-        <p>Import Component</p>
-    )
-}
-
-export default Import
+export default Import;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import state from './state'
 import thunk from 'redux-thunk'
-
-export default createStore(reducers, state)
-
-
-
-
-
-
-
-
-
 
 
 
